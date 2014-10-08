be568b71732b810f288af5703a8c3b91057f7f05
Qing-Cheng Li <qc.linux@gmail.com>
2014-10-08 12:09:03 +0800

    nodify message and add link share

--- fb.py
+++ fb.py
@@ -3,15 +3,32 @@
 
 import facebook
 import json
+from datetime import datetime
 
 def postStatusOnFacebook(accessToken):
-   
-    graph = facebook.GraphAPI(accessToken)
-
+    
+    # get stored status.
     status = json.load(open("./status.json","r"))
+    
+    today = datetime.now()
+    postIndex = today.day - 8
 
-    result = graph.put_object("me","feed",message=status[0]["message"])
+    if postIndex < 0 or postIndex >= len(status):
+        print "There is no content."
+        return
 
+    post = status[postIndex]
+
+    # create facebook GraphAPI
+    graph = facebook.GraphAPI(accessToken)
+    if "link" in post:
+        result = graph.put_object("me","feed",message=post["message"],link=post["link"])
+        pass
+    else:
+        result = graph.put_object("me","feed",message=post["message"])
+        pass
+    
+    print "Posted, result=",result
 
 if __name__ == "__main__":
     fbSetting = json.load(open("./fb.json","r"))
